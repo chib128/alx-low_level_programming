ec78476a5fabf297183b3a4f0bedbfdbaabfe9cc
chib128 <brianchibisa2@gmail.com>
2023-02-22 08:36:39 -0800

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 
 
 /**
- * _islower - checks for lowercase character
+ * _islower - Checks for lowercase character
  * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
-int _islower(int c)
+int _isalpha(int c)
 
 {
         if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
