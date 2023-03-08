09180119eb10d1def1e7d4b61359c45ab91629df
chib128 <brianchibisa2@gmail.com>
2023-03-08 07:19:11 -0800

    #include main.h

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion-function like  puts();
+ * @a: inpurt
+ * Return: Always B (success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar("\n");
+}
